d22821b884b1e660c4e66f75fa3ebe4704f3d0f4
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2023-09-19 18:23:38 -0400

    Update to latest Zig, remove example bloat

--- build.zig
+++ build.zig
@@ -15,49 +15,36 @@ pub fn build(b: *std.Build) void {
 
     // Example
 
-    const exe = b.addExecutable(.{
-        .name = "treez-example",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "example.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+//     const exe = b.addExecutable(.{
+//         .name = "treez-example",
+//         // In this case the main source file is merely a path, however, in more
+//         // complicated build scripts, this could be a generated file.
+//         .root_source_file = .{ .path = "example.zig" },
+//         .target = target,
+//         .optimize = optimize,
+//     });
 
-    exe.linkLibC();
+//     exe.linkLibC();
 
-    exe.linkLibrary(b.dependency("tree-sitter", .{
-        .target = target,
-        .optimize = optimize,
-    }).artifact("tree-sitter"));
+//     exe.linkLibrary(b.dependency("tree-sitter", .{
+//         .target = target,
+//         .optimize = optimize,
+//     }).artifact("tree-sitter"));
 
-    exe.linkLibrary(b.dependency("tree-sitter-zig", .{
-        .target = target,
-        .optimize = optimize,
-    }).artifact("tree-sitter-zig"));
+//     exe.linkLibrary(b.dependency("tree-sitter-zig", .{
+//         .target = target,
+//         .optimize = optimize,
+//     }).artifact("tree-sitter-zig"));
 
-    b.installArtifact(exe);
+//     b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
+//     const run_cmd = b.addRunArtifact(exe);
+//     run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+//     if (b.args) |args| {
+//         run_cmd.addArgs(args);
+//     }
 
-    const run_step = b.step("example", "Run the example");
-    run_step.dependOn(&run_cmd.step);
-
-    // Tests
-
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "treez.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+//     const run_step = b.step("example", "Run the example");
+//     run_step.dependOn(&run_cmd.step);
 }
